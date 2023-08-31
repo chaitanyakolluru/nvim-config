6c15b4fb1d8629dc80a07936d70c7e4a96d2050b
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2023-08-31 08:50:34 -0700

    adding treesj and nvim-surround

--- lua/chai/packer.lua
+++ lua/chai/packer.lua
@@ -61,4 +61,44 @@ return require('packer').startup(function(use)
     'nvim-tree/nvim-web-devicons', -- optional
   },
 }
+
+use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
+
+use({
+  'Wansmer/treesj',
+  requires = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesj').setup({
+    ---@type boolean Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
+  use_default_keymaps = true,
+  ---@type boolean Node with syntax error will not be formatted
+  check_syntax_error = true,
+  ---If line after join will be longer than max value,
+  ---@type number If line after join will be longer than max value, node will not be formatted
+  max_join_length = 2400,
+  ---Cursor behavior:
+  ---hold - cursor follows the node/place on which it was called
+  ---start - cursor jumps to the first symbol of the node being formatted
+  ---end - cursor jumps to the last symbol of the node being formatted
+  ---@type 'hold'|'start'|'end'
+  cursor_behavior = 'hold',
+  ---@type boolean Notify about possible problems or not
+  notify = true,
+  ---@type table Presets for languages
+  ---@type boolean Use `dot` for repeat action
+  dot_repeat = true,
+  ---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
+  on_error = nil,
+})
+  end,
+})
+
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -164,6 +164,12 @@ _G.packer_plugins = {
     path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
+  ["nvim-surround"] = {
+    config = { "\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0" },
+    loaded = true,
+    path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/nvim-surround",
+    url = "https://github.com/kylechui/nvim-surround"
+  },
   ["nvim-tree.lua"] = {
     loaded = true,
     path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
@@ -199,6 +205,12 @@ _G.packer_plugins = {
     path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  treesj = {
+    config = { "\27LJ\2\n \1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\6\23check_syntax_error\2\24use_default_keymaps\2\vnotify\2\15dot_repeat\2\20cursor_behavior\thold\20max_join_length\3à\18\nsetup\vtreesj\frequire\0" },
+    loaded = true,
+    path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/treesj",
+    url = "https://github.com/Wansmer/treesj"
+  },
   undotree = {
     loaded = true,
     path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/undotree",
@@ -222,6 +234,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-surround
+time([[Config for nvim-surround]], true)
+try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
+time([[Config for nvim-surround]], false)
+-- Config for: treesj
+time([[Config for treesj]], true)
+try_loadstring("\27LJ\2\n \1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\6\23check_syntax_error\2\24use_default_keymaps\2\vnotify\2\15dot_repeat\2\20cursor_behavior\thold\20max_join_length\3à\18\nsetup\vtreesj\frequire\0", "config", "treesj")
+time([[Config for treesj]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
