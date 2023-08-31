20d8e684eee798b06a021531ba4cb1eafa810268
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2023-08-31 11:30:23 -0700

    adding trouble.nvim

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -206,10 +206,15 @@ _G.packer_plugins = {
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   treesj = {
-    config = { "\27LJ\2\n \1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\6\23check_syntax_error\2\24use_default_keymaps\2\vnotify\2\15dot_repeat\2\20cursor_behavior\thold\20max_join_length\3à\18\nsetup\vtreesj\frequire\0" },
+    config = { "\27LJ\2\n \1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\6\24use_default_keymaps\2\vnotify\2\15dot_repeat\2\20cursor_behavior\thold\20max_join_length\3à\18\23check_syntax_error\2\nsetup\vtreesj\frequire\0" },
     loaded = true,
     path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/treesj",
     url = "https://github.com/Wansmer/treesj"
+  },
+  ["trouble.nvim"] = {
+    loaded = true,
+    path = "/Users/k387899/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    url = "https://github.com/folke/trouble.nvim"
   },
   undotree = {
     loaded = true,
@@ -234,14 +239,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: treesj
+time([[Config for treesj]], true)
+try_loadstring("\27LJ\2\n \1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\6\24use_default_keymaps\2\vnotify\2\15dot_repeat\2\20cursor_behavior\thold\20max_join_length\3à\18\23check_syntax_error\2\nsetup\vtreesj\frequire\0", "config", "treesj")
+time([[Config for treesj]], false)
 -- Config for: nvim-surround
 time([[Config for nvim-surround]], true)
 try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
 time([[Config for nvim-surround]], false)
--- Config for: treesj
-time([[Config for treesj]], true)
-try_loadstring("\27LJ\2\n \1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\6\23check_syntax_error\2\24use_default_keymaps\2\vnotify\2\15dot_repeat\2\20cursor_behavior\thold\20max_join_length\3à\18\nsetup\vtreesj\frequire\0", "config", "treesj")
-time([[Config for treesj]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
