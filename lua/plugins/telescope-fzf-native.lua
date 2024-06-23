2b2897a13cb702dae50a7aede5a6d52c4a9b5c55
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2024-06-22 22:33:22 -0500

    perfection

--- lua/plugins/telescope-fzf-native.lua
+++ lua/plugins/telescope-fzf-native.lua
@@ -1,5 +1,7 @@
 local Plugin = { 'nvim-telescope/telescope-fzf-native.nvim' }
 
+Plugin.lazy = true
+
 Plugin.build = {
   'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,32 +4,31 @@ Plugin.version = '0.1.8'
 
 Plugin.dependencies = {
   { 'nvim-lua/plenary.nvim' },
+  { 'nvim-telescope/telescope-fzf-native.nvim' },
 }
 
+Plugin.keys = {
+  --  -- file pickers
+  { '<leader>sf', ':Telescope find_files<CR>', desc = "[S]earch [F]iles" },
+  { '<leader>sg', ':Telescope git_files<CR>', desc = "Search [G]it [F]iles" },
+  { '<leader>sG', ':Telescope live_grep<CR>', desc = "[S]earch by [G]rep" },
+  { '<leader>sw', ':Telescope grep_string<CR>', desc = "[S]earch current [W]ord" },
 
-function Plugin.config()
-  -- file pickers
-  vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-  vim.keymap.set('n', '<leader>sg', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-  vim.keymap.set('n', '<leader>sG', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+  --  -- vim pickers
+  { '<leader>sh', ':Telescope help_tags<CR>', desc = "[S]earch [H]elp" },
+  { '<leader>sb', ':Telescope buffers<CR>', desc = "[S]earch [B]uffers" },
+  { '<leader>sm', ':Telescope marks<CR>', desc = "[S]earch [M]arks" },
+  { '<leader>sC', ':Telescope colorscheme<CR>', desc = "[S]earch [C]olorscheme" },
+  { '<leader>sc', ':Telescope commands<CR>', desc = "[S]earch [C]ommands" },
+  { '<leader>sH', ':Telescope highlights<CR>', desc = "[S]earch [H]ighlights" },
 
-  -- vim pickers
-  vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
-  vim.keymap.set('n', '<leader>sm', require('telescope.builtin').marks, { desc = '[S]earch [M]arks' })
-  vim.keymap.set('n', '<leader>sC', require('telescope.builtin').colorscheme, { desc = '[S]earch [C]olorscheme' })
-  vim.keymap.set('n', '<leader>sc', require('telescope.builtin').commands, { desc = '[S]earch [C]ommands' })
-  vim.keymap.set('n', '<leader>sH', require('telescope.builtin').highlights, { desc = '[S]earch [H]ighlights' })
+  --  -- neovim lsp pickers
+  { 'gr', ':Telescope lsp_references<CR>', desc = "[G]oto [R]eferences" },
+  { '<leader>ds', ':Telescope lsp_document_symbols<CR>', desc = "[D]ocument [S]ymbols" },
+  { '<leader>ws', ':Telescope lsp_workspace_symbols<CR>', desc = "[W]orkspace [S]ymbols" },
+  { '<leader>sd', ':Telescope diagnostics<CR>', desc = "[S]earch [D]iagnostics" },
+}
 
-
-  -- neovim lsp pickers
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
-  vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = '[D]ocument [S]ymbols' })
-  vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-    { desc = '[W]orkspace [S]ymbols' })
-  vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-
-end
+Plugin.config = true
 
 return Plugin
