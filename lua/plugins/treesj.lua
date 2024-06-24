d8b146e9765ea84d4263a7a653575dd8d1670450
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2024-06-24 11:03:51 -0500

    adding in treesj with lazy options

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,12 @@
+local Plugin = { 'Wansmer/treesj' }
+
+
+Plugin.keys = { '<space>m', '<space>j', '<space>s' }
+
+Plugin.dependencies = { 'nvim-treesitter/nvim-treesitter' }
+
+function Plugin.config()
+    require('treesj').setup({ --[[ your config ]] })
+end
+
+return Plugin
