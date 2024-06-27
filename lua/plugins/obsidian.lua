e6b8f697a2aad1c22649faaf108e0f3ffa172365
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2024-06-27 11:39:26 -0500

    adding keymaps for obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -4,6 +4,15 @@ Plugin.dependencies = {
   { 'nvim-lua/plenary.nvim' },
 }
 
+Plugin.keys = {
+  { '<leader>ofl', ':ObsidianFollowLink<CR>', desc = "Obsidian Follow Link" },
+  { '<leader>obl', ':ObsidianBacklinks<CR>', desc = "Obsidian Back Link" },
+  { '<leader>ol', ':ObsidianLinks<CR>', desc = "Obsidian Links" },
+  { '<leader>on', ':ObsidianNew<CR>', desc = "Obsidian Links" },
+  -- :ObsidianLink with the id or alias as a command on a visual selection to
+  -- link the selection to the other md with the identifier
+}
+
 Plugin.version = "*"
 Plugin.lazy = true
 Plugin.ft = "markdown"
