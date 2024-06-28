0b66c6126afdf2cfc88d08b91c2f00a39a5aa04f
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2024-06-28 13:08:48 -0500

    following links, other keymaps being set

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,10 +6,10 @@ Plugin.dependencies = {
 
 Plugin.keys = {
   { '<leader>ofl', ':ObsidianFollowLink<CR>', desc = "Obsidian Follow Link" },
-  { '<leader>obl', ':ObsidianBacklinks<CR>', desc = "Obsidian Back Link" },
-  { '<leader>ol', ':ObsidianLinks<CR>', desc = "Obsidian Links" },
-  { '<leader>on', ':ObsidianNew<CR>', desc = "Obsidian Links" },
-  { '<leader>os', ':ObsidianSearch<CR>', desc = "Obsidian Search" },
+  { '<leader>obl', ':ObsidianBacklinks<CR>',  desc = "Obsidian Back Link" },
+  { '<leader>ol',  ':ObsidianLinks<CR>',      desc = "Obsidian Links" },
+  { '<leader>on',  ':ObsidianNew<CR>',        desc = "Obsidian New" },
+  { '<leader>os',  ':ObsidianSearch<CR>',     desc = "Obsidian Search" },
   -- :ObsidianLink with the id or alias as a command on a visual selection to
   -- link the selection to the other md with the identifier
 }
@@ -26,6 +26,14 @@ Plugin.opts = {
       path = "/Users/k387899/Documents/obsidian-vault",
     },
   },
+  -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+  -- URL it will be ignored but you can customize this behavior here.
+  ---@param url string
+  follow_url_func = function(url)
+    -- Open the URL in the default web browser.
+    vim.fn.jobstart({ "open", url }) -- Mac OS
+    -- vim.fn.jobstart({"xdg-open", url})  -- linux
+  end,
 }
 
 return Plugin
