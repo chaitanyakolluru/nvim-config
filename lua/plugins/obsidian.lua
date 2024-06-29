43dfcc75b5cfcd7b186cb142616c8e3d55cceaaa
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2024-06-29 13:19:53 -0500

    oil c-h unmap; obs enter unmap

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -34,6 +34,25 @@ Plugin.opts = {
     vim.fn.jobstart({ "open", url }) -- Mac OS
     -- vim.fn.jobstart({"xdg-open", url})  -- linux
   end,
+  mappings = {
+    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+    -- Toggle check-boxes.
+    ["<leader>ch"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+    -- Smart action depending on context, either follow link or toggle checkbox.
+    -- turning this off
+    ["<cr>"] = { "false" }
+  },
 }
 
 return Plugin
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -21,7 +21,7 @@ function Plugin.config()
       ["g?"] = "actions.show_help",
       ["<CR>"] = "actions.select",
       ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-      ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+      ["<C-h>"] = { false },
       ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
       ["<C-p>"] = "actions.preview",
       ["<C-c>"] = "actions.close",
