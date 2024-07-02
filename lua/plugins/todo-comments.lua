6c55a801e60ddeb467020e201ef28ebdc0d31357
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2024-07-02 16:14:05 -0500

    pattern matching reverted on todo comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -64,8 +64,8 @@ Plugin.opts = {
     },
     -- regex that will be used to match keywords.
     -- don't replace the (KEYWORDS) placeholder
-    -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-    pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
   }
 }
 
