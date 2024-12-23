83dc1a8adcb4d6ff91774593cb82930d060333f7
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2024-12-23 17:00:24 -0600

    adding in lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,18 @@
+local Plugin = { 'kdheepak/lazygit.nvim' }
+
+Plugin.keys = {
+  { "<leader>lg", ':LazyGit<CR>', desc = "Open Lazy git" },
+}
+
+Plugin.cmd = {
+  "LazyGit",
+  "LazyGitConfig",
+  "LazyGitCurrentFile",
+  "LazyGitFilter",
+  "LazyGitFilterCurrentFile",
+}
+
+Plugin.dependencies = { "nvim-lua/plenary.nvim" }
+
+return Plugin
+
