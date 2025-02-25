2a7d425cf4beefb8a72d30b1f5cc24a185b92332
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2025-02-24 18:23:10 -0600

    Revert "removing vim-go"
    This reverts commit 35659c86f6e30d045a7b0c3ce16a8650f856337c.

---
+++ lua/plugins/vim-go.lua
@@ -0,0 +1,10 @@
+local Plugin = { 'fatih/vim-go' }
+
+-- This is a list of filetypes that will be used to determine if the plugin should be loaded.
+Plugin.ft = { "go" }
+
+Plugin.config = function()
+end
+
+
+return Plugin
