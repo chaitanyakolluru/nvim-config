885e11ee0f8bcbc4897f11081aa41d97576ad2c2
Chaitanya Kolluru <chaitanya.kolluru@heb.com>
2025-03-05 10:11:18 -0600

    cleaning up unneeded plugins in snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,26 +4,26 @@ Snacks = {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    -- bigfile = { enabled = true },
+    -- dashboard = { enabled = true },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
-    notifier = {
-      enabled = true,
-      timeout = 3000,
-    },
+    -- notifier = {
+    --   enabled = true,
+    --   timeout = 3000,
+    -- },
     picker = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
     -- scroll = { enabled = true },
-    statuscolumn = { enabled = true },
+    -- statuscolumn = { enabled = true },
     words = { enabled = true },
-    styles = {
-      notification = {
-        -- wo = { wrap = true } -- Wrap notifications
-      }
-    }
+    -- styles = {
+    --   notification = {
+    --     -- wo = { wrap = true } -- Wrap notifications
+    --   }
+    -- }
   },
   keys = {
     -- Top Pickers & Explorer
